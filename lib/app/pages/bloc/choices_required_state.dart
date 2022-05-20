a5a8d90dea5cd30a49aac44ce85915729ea3c4a0
murilinhops <murilo.pescatori@nubank.com.br>
2022-05-20 16:10:16 -0300

    refactor

--- lib/app/pages/bloc/choices_required_state.dart
+++ lib/app/pages/bloc/choices_required_state.dart
@@ -2,27 +2,19 @@ import 'package:flutter/widgets.dart';
 import 'package:text_adventure_app/app/shared/models/model.dart';
 
 class ChoicesRequiredState {
-  bool _requiredStateKeys = false;
-
-  bool verifyRequiredStateKeys(Options options, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+  bool verifyRequiredStateKeys(
+      Options options, AsyncSnapshot<Map<String, dynamic>> snapshot) {
     var newMapListFromSnapshot = List<Map<String, dynamic>>();
 
-    if (snapshot.hasData)
-      snapshot.data.forEach((key, value) => newMapListFromSnapshot.add({key: value}));
+    if (!snapshot.hasData) return false;
 
-    newMapListFromSnapshot.forEach((requiredState) {
-      if (options.requiredState.toString() == requiredState.toString()) {
-        print("Element exists in newMapListFromSnapshot $requiredState");
+    snapshot.data
+        .forEach((key, value) => newMapListFromSnapshot.add({key: value}));
 
-        _requiredStateKeys = true;
-      } else {
-        print("Element doesn't exist in newMapListFromSnapshot $requiredState");
-
-        _requiredStateKeys = false;
-      }
-    });
-
-    return _requiredStateKeys;
+    return newMapListFromSnapshot.any(
+      (requiredState) =>
+          options.requiredState.toString() == requiredState.toString(),
+    );
   }
 }
 
